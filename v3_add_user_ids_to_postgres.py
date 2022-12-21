b6984ad90f1fa53420c252ced2aa244774e158d4
DS-jr <dmitry.k.sokolov@gmail.com>
2022-12-21 20:48:19 +0400

    Minor changes. Made hidden updates locally

---
+++ v3_add_user_ids_to_postgres.py
@@ -0,0 +1,10 @@
+import json
+
+with open("export_result_TG_account_N4.json") as json_export_file:
+    dict_3 = json.load(json_export_file)
+    # print(type(dict_3))
+    # a_1 = json_export.read()
+    # print(type(a_1))
+
+# print(dict_3["contacts"]["list"][0]["phone_number"])
+
