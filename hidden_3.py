2dd96a8b47cfb7fb817a57df373b34faa1ffe596
DS-jr <dmitry.k.sokolov@gmail.com>
2022-12-22 20:41:21 +0400

    Connected to Postgres via hidden file. Inserted values to DB with Python script

---
+++ hidden_3.py
@@ -0,0 +1,7 @@
+
+def secrets_3():
+    return {"host": "localhost",
+            "port": 5432,
+            "database": "v1_tg_data_export",
+            "user": "DS"
+            }
--- v3_add_user_ids_to_postgres.py
+++ v3_add_user_ids_to_postgres.py
@@ -1,4 +1,5 @@
-import json
+import json, psycopg2
+import hidden_3
 
 with open("export_result_TG_account_N4.json") as json_export_file:
     dict_3 = json.load(json_export_file)
@@ -6,5 +7,24 @@ with open("export_result_TG_account_N4.json") as json_export_file:
     # a_1 = json_export.read()
     # print(type(a_1))
 
-# print(dict_3["contacts"]["list"][0]["phone_number"])
+for k in dict_3["chats"]["list"]:
+    print(k["id"])
 
+connection_3 = psycopg2.connect(
+    host=hidden_3.secrets_3()["host"],
+    port=hidden_3.secrets_3()["port"],
+    database=hidden_3.secrets_3()["database"],
+    user=hidden_3.secrets_3()["user"]
+)
+
+cur_3 = connection_3.cursor()
+
+
+# var_1 = "INSERT INTO contacts_users (tg_ids) VALUES (78901);"
+# cur_3.execute(var_1)
+# connection_3.commit()
+
+# (via Python) Add manually a new row to an existing table in the DB
+
+#a_1 = dict_3["chats"]["list"][1]["id"]
+#print(a_1)
