7f6feda64c6cf4fd6cf1835b2872d0315153ef24
DS-jr <dmitry.k.sokolov@gmail.com>
2023-01-05 21:08:21 +0400

    updated v5_parse_telegram_data.py

--- v5_parse_telegram_data.py
+++ v5_parse_telegram_data.py
@@ -10,22 +10,27 @@ engine = create_engine(config.DATABASE_STRING, future=True)  # Engine is a facto
 
 # insert chat data (telegram_id, name, type) into SQLite:
 with engine.connect() as con_5:
+
     for chat in exported_data_dict["chats"]["list"]:
-        var_1 = text()  # (?) Is 'text' module vital here?
+        var_1 = text(  # (?) Try to delete 'text' module: is it vital here?
+        # (?!) Test what happens if I do NOT use colons in line 18
         """
         INSERT INTO telegram_chats (name, type, telegram_id,) 
-        VALUES(:name, :type, :id) 
+        VALUES(:name_1, :type_1, :id_1)   
         ON CONFLICT (telegram_id) DO NOTHING
         """
         )
-        # con_5.execute(var_1, {..??..})   # Proceed from here!
+        con_5.execute(var_1, {"id_1": chat["id"], "name_1": chat["name"], "type_1": chat["type"]})
+
+        for message in chat["messages"]:
+            var_2 = text(
+            # Proceed from here!
+            )
+
+
+
 
         # print(chat["type"])
-
-
-
-
-
     # print(type(exported_data_dict["chats"]["list"]))
     # ...??...
 
