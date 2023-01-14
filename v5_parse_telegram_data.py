9f29586f3da2eab301367c2511079d604d884b6b
DS-jr <dmitry.k.sokolov@gmail.com>
2023-01-14 18:58:58 +0700

    fixing SQLite errors

--- v5_parse_telegram_data.py
+++ v5_parse_telegram_data.py
@@ -21,19 +21,27 @@ with engine.connect() as con_5:
     con_5.execute(delete_all)
 
     for chat in exported_data_dict["chats"]["list"]:
-        # insert_5 = TelegramChat().insert().values(telegram_id = chat["id"],  name = chat.get("name"), type = chat["type"])
         insert_5 = insert(TelegramChat).values(telegram_id=chat["id"], name=chat.get("name"), type=chat["type"])
         result_5 = con_5.execute(insert_5)
 
         for message in chat["messages"]:
-            insert_51 = insert(TelegramMessage).values(telegram_id=message["id"], date=dateutil.parser.parse(message["date"]), unix_timestamp=message["date_unixtime"], from_name=message.get("from"), from_id=message.get("from_id"), chat_id=chat["id"])
+            insert_51 = insert(TelegramMessage).values(telegram_id=message["id"], date=dateutil.parser.parse(message["date"]), unix_timestamp=message["date_unixtime"], from_name=message.get("from"), from_id=message.get("from_id"), chat_id=chat["id"]) # (?) (error: NOT NULL constraint failed: telegram_message.id ) (hypothesis) Auto-increment is NOT working in "id" column
+#            insert_51 = insert(TelegramMessage).values(id=message["id"], telegram_id=message["id"], date=dateutil.parser.parse(message["date"]), unix_timestamp=message["date_unixtime"], from_name=message.get("from"), from_id=message.get("from_id"), chat_id=chat["id"]) # This "crutch" experimental draft variant worked w/ NO errors
             result_51 = con_5.execute(insert_51)
+
+#        id = chat.["id"]  # did NOT work
+#        id = chat.get("id")  # did NOT work
+#        telegram_id = message["id"]  # Old variant that did NOT work
+
 
         # date = message["date"]
         # telegram_id, date, unix_timestamp, from_name, from_id, chat_id
         #             con_5.execute(var_2, {"id": message["id"], "date": message["date"], "unix_timestamp": message["date_unixtime"], "from_name": message.get("from"), "from_id": message.get("from_id"), "chat_id": chat["id"]},)
         # for message in chat["messages"]:
 
+
+
+        # insert_5 = TelegramChat().insert().values(telegram_id = chat["id"], name = chat.get("name"), type = chat["type"])
 
         # for chat in exported_data_dict["chats"]["list"]:
 #         var_1 = text(
