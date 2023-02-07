cdf32f79449473ae9f749450af63a0654f039201
DS-jr <dmitry.k.sokolov@gmail.com>
2023-02-07 20:25:17 +0700

    added updating option for TelegramChat

--- v5_parse_telegram_data.py
+++ v5_parse_telegram_data.py
@@ -1,6 +1,6 @@
 import json, config, dateutil.parser
-from sqlalchemy import create_engine, insert, delete
-from sqlalchemy.sql import text
+from sqlalchemy import create_engine, insert, delete, update, text
+# from sqlalchemy.sql import text
 from create_tables_in_DB import TelegramChat, TelegramMessage
 
 with open("export_result_TG_account_N4.json") as json_file:
@@ -19,16 +19,16 @@ with engine.connect() as con_5:
         if not chat["messages"]:
             continue
 
+        messages_min_date_confirmed = chat["messages"][0]["date"]
+
         insert_5 = insert(TelegramChat).values(
             telegram_id=chat["id"],
             name=chat.get("name"),
             type=chat["type"],
             first_date_from_the_left_side_in_messages_list=chat["messages"][0]["date"],
+            # messages_min_date_checked=messages_min_date_confirmed,  # This line can be used if we prove: first_date_from_the_left_side_in_messages_list == messages_min_date_checked in ALL cases
         )  # (add?) dateutil.parser.parse
-
-        result_5 = con_5.execute(insert_5)
-
-        messages_min_date_confirmed = chat["messages"][0]["date"]
+        con_5.execute(insert_5)
 
         for message in chat["messages"]:
             insert_51 = insert(TelegramMessage).values(
@@ -39,17 +39,26 @@ with engine.connect() as con_5:
                 from_id=message.get("from_id"),
                 chat_id=chat["id"],
             )  # (?) (error: NOT NULL constraint failed: telegram_message.id ) (hypothesis) Auto-increment is NOT working in "id" column
-            result_51 = con_5.execute(insert_51)
+            con_5.execute(insert_51)
             if message["date"] < messages_min_date_confirmed:
                 messages_min_date_confirmed = message["date"]
                 print(messages_min_date_confirmed, "(inside the second 'for' cycle)") # (CDL) To confirm the 'if' condition above is never executed
 
-        insert_6 = insert(TelegramChat).values(
-            messages_min_date_checked=messages_min_date_confirmed
-        )
-        result_6 = con_5.execute(insert_6)
+        # (CDL)  UPDATE instead of INSERT
+        # This block could be deleted in case we prove the "if" condition above is NOT necessary (aka can also be deleted)
+        u = update(TelegramChat)
+        updated_column_name = TelegramChat.messages_min_date_checked
+        val = u.values({"updated_column_name":"messages_min_date_confirmed"})
+        cond = val.where(TelegramChat.c.updated_column_name == "NULL")   #  Error: AttributeError: type object 'TelegramChat' has no attribute 'c'
+        con_5.execute(cond)
+
+        # insert_6 = insert(TelegramChat).values(
+        #     messages_min_date_checked=messages_min_date_confirmed
+        # )
+        # result_6 = con_5.execute(insert_6)
 
         # print(messages_min_date_confirmed)
+
         con_5.commit()
 
         #            insert_51 = insert(TelegramMessage).values(id=message["id"], telegram_id=message["id"], date=dateutil.parser.parse(message["date"]), unix_timestamp=message["date_unixtime"], from_name=message.get("from"), from_id=message.get("from_id"), chat_id=chat["id"]) # This "crutch" experimental draft variant worked w/ NO errors
